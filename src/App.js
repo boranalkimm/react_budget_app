447be8b5ec4f1c0f028e775e2cc3affd84b9d4f6
Boran Erdogan <boranalkimm@gmail.com>
2024-06-25 12:06:17 -0400

    Up until individual expense item script

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 //Code to import Budget.js
 import Budget from './components/Budget';
+import Remaining from './components/Remaining';
+import ExpenseTotal from './components/ExpenseTotal';
 
 // Add code to import the other components here under
 
@@ -17,14 +19,26 @@ const App = () => {
                     <div className='row mt-3'>
                         {
                             /* Add Budget component here */
+                                            // Budget component
+                    <div className='col-sm'>
+                        <Budget />
+                    </div>
                         }        
 
                         {
                             /* Add Remaining component here*/
+                                             //Remaining component
+                    <div className='col-sm'>
+                        <Remaining />
+                    </div>
                         }        
 
                         {
                             /* Add ExpenseTotal component here */
+                                             //ExpenseTotal component
+                    <div className='col-sm'>
+                        <ExpenseTotal />
+                    </div>
                         }        
                        
                         {
